4793724888f74b1233d65e29162f04c0be79b356
priyanshusingh99-creator <priyanshusinghcktd@gmail.com>
2026-08-11 14:28:33 +0530

    fix express export and yargs check for vercel

--- backend-main/index.js
+++ backend-main/index.js
@@ -18,71 +18,28 @@ const { revertRepo } = require("./controllers/revert");
 
 dotenv.config();
 
-yargs(hideBin(process.argv))
-  .command("start", "Starts a new server", {}, startServer)
-  .command("init", "Initialise a new repository", {}, initRepo)
-  .command(
-    "add <file>",
-    "Add a file to the repository",
-    (yargs) => {
-      yargs.positional("file", {
-        describe: "File to add to the staging area",
-        type: "string",
-      });
-    },
-    (argv) => {
-      addRepo(argv.file);
-    }
-  )
-  .command(
-    "commit <message>",
-    "Commit the staged files",
-    (yargs) => {
-      yargs.positional("message", {
-        describe: "Commit message",
-        type: "string",
-      });
-    },
-    (argv) => {
-      commitRepo(argv.message);
-    }
-  )
-  .command("push", "Push commits to S3", {}, pushRepo)
-  .command("pull", "Pull commits from S3", {}, pullRepo)
-  .command(
-    "revert <commitID>",
-    "Revert to a specific commit",
-    (yargs) => {
-      yargs.positional("commitID", {
-        describe: "Comit ID to revert to",
-        type: "string",
-      });
-    },
-    (argv) => {
-      revertRepo(argv.commitID);
-    }
-  )
-  .demandCommand(1, "You need at least one command")
-  .help().argv;
+// 1. Initialize Express App at top-level for Vercel
+const app = express();
 
+app.use(express.json());
+app.use(cors({ origin: "*" }));
+
+// Database Connection
+const mongoURI =
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/githubclone";
+
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log("MongoDB connected!"))
+  .catch((err) => console.error("Unable to connect : ", err));
+
+// Routes
+app.use("/", mainRouter);
+
+// Local Server Runner
 function startServer() {
-  const app = express();
   const port = process.env.PORT || 3002;
   const host = process.env.HOST || "127.0.0.1";
-
-  app.use(express.json());
-
-  const mongoURI =
-    process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/githubclone";
-
-  mongoose
-    .connect(mongoURI)
-    .then(() => console.log("MongoDB connected!"))
-    .catch((err) => console.error("Unable to connect : ", err));
-
-  app.use(cors({ origin: "*" }));
-
-  app.use("/", mainRouter);
 
   let user = "test";
   const httpServer = http.createServer(app);
@@ -104,13 +61,64 @@ function startServer() {
   });
 
   const db = mongoose.connection;
-
   db.once("open", async () => {
     console.log("CRUD operations called");
-    // CRUD operations
   });
 
   httpServer.listen(port, host, () => {
     console.log(`Server is running at http://${host}:${port}`);
   });
-}module.exports = app;
+}
+
+// 2. Run Yargs CLI only locally (bypasses Vercel deployment check)
+if (!process.env.VERCEL && process.argv.length > 2) {
+  yargs(hideBin(process.argv))
+    .command("start", "Starts a new server", {}, startServer)
+    .command("init", "Initialise a new repository", {}, initRepo)
+    .command(
+      "add <file>",
+      "Add a file to the repository",
+      (yargs) => {
+        yargs.positional("file", {
+          describe: "File to add to the staging area",
+          type: "string",
+        });
+      },
+      (argv) => {
+        addRepo(argv.file);
+      }
+    )
+    .command(
+      "commit <message>",
+      "Commit the staged files",
+      (yargs) => {
+        yargs.positional("message", {
+          describe: "Commit message",
+          type: "string",
+        });
+      },
+      (argv) => {
+        commitRepo(argv.message);
+      }
+    )
+    .command("push", "Push commits to S3", {}, pushRepo)
+    .command("pull", "Pull commits from S3", {}, pullRepo)
+    .command(
+      "revert <commitID>",
+      "Revert to a specific commit",
+      (yargs) => {
+        yargs.positional("commitID", {
+          describe: "Comit ID to revert to",
+          type: "string",
+        });
+      },
+      (argv) => {
+        revertRepo(argv.commitID);
+      }
+    )
+    .demandCommand(1, "You need at least one command")
+    .help().argv;
+}
+
+// 3. Export Express App for Vercel
+module.exports = app;
